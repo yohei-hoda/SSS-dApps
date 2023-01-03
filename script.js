114fe299bdbf48e3eeccc3a35fabb9cc9916cb19
yohei-hoda <giga200american@gmail.com>
2023-01-03 17:40:48 +0900

    1740変更

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const symbol = require('/node_modules/symbol-sdk')
 
 const address = symbol.Address.createFromRawAddress("TCKFIYOFJBBDMYCXCWMT5XNWVRVBYFF6F7PNFJY")
 
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
+
 console.log("Hello Symbol")
 console.log(`Your Address : ${address.plain()}`)
