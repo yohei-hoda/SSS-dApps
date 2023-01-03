7dcbc8f955cca4f464ee973683d50150329a4318
yohei-hoda <giga200american@gmail.com>
2023-01-03 17:18:35 +0900

    js変更

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TCKFIYOFJBBDMYCXCWMT5XNWVRVBYFF6F7PNFJY")
+
 console.log("Hello Symbol")
+console.log('Your Address : ${address.plain()}')
