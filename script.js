3e480d81523f5ef8990d8d8eb82fa0fd9e967480
yohei-hoda <giga200american@gmail.com>
2023-01-03 17:28:36 +0900

    修正

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const symbol = require('/node_modules/symbol-sdk')
 const address = symbol.Address.createFromRawAddress("TCKFIYOFJBBDMYCXCWMT5XNWVRVBYFF6F7PNFJY")
 
 console.log("Hello Symbol")
-console.log('Your Address : ${address.plain()}')
+console.log(`Your Address : ${address.plain()}`)
